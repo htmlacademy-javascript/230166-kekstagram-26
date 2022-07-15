3932c8fb12d079cb4cd8d08853aece6e3a3b7879
Lipsum <m.margaritapopova@gmail.com>
2022-07-15 12:18:27 +0300

    Удаляет лишние функции

--- js/utils.js
+++ js/utils.js
@@ -1,19 +1,11 @@
 const isEscapeKey = (e) => e.key === 'Escape';
 
-function getRandomPositiveInteger (a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+function getRandomPositiveInteger(min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
-}
-
-function checkLength (string, len = 10) {
-  return string.length <= len;
-}
-
-function getRandomArrayElement(elements) {
-  return elements[getRandomPositiveInteger(0, elements.length - 1)];
 }
 
 function chunkArray(array, chunkSize) {
@@ -46,10 +38,8 @@ function debounce(callback, timeoutDelay) {
 }
 
 export {
-  getRandomPositiveInteger,
-  checkLength,
-  getRandomArrayElement,
-  isEscapeKey, chunkArray,
+  isEscapeKey,
+  chunkArray,
   getRandomSet,
   debounce,
 };
